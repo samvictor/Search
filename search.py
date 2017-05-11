800899b4935e2725866e52256a121ae44721bb6e
Sam <sjinniss1@hotmail.com>
2017-05-11 12:49:27 -0500

    Initial Commit

--- search.py
+++ search.py
@@ -1,17 +1,30 @@
 #Search Script
 from os import walk, path
 
-s_path = input('Search where? (enter "~" for user directory)\n')
+input_str = input('Search where? (enter "~" for user directory)\n')
 
 walk_data = ()
-if s_path == '~':
-    walk_data = next(walk (path.expanduser('~')))
-else:
-    walk_data = next(walk(path))
+while True:
+    if input_str == '~':
+        walk_data = next(walk (path.expanduser('~')))
+    else:
+        walk_data = next(walk(input_str))
 
-print ('Looking in: ')
-print('\t' + walk_data[0])
-print ('\nFolders:')
-print ('\t' + ', '.join(walk_data[1]))
-print ('\nFiles:')
-print ('\t' + ', '.join(walk_data[2]))
+    print ('Looking in: ')
+    print ('\t' + walk_data[0])
+    print ('\nFolders:')
+    print ('\t' + ', '.join(walk_data[1]))
+    print ('\nFiles:')
+    print ('\t' + ', '.join(walk_data[2]))
+
+    input_str = input('\nNext? (enter path name or -s to search, enter ~ for user directory, enter c:/ for root)\n')
+    if input_str == '-s':
+        break
+    elif input_str == '~':
+        pass
+    elif input_str in ['c:/', 'C:/', 'c:', 'C:']:
+        pass
+    else:
+        input_str = walk_data[0] + input_str
+
+input('\nEnter search term:\n')
